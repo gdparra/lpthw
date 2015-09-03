09eb3aba34e681c74b7acacc32bf98e80fa273ae
Gonzalo <gonzalo_delatorre@live.com>
2015-09-03 12:50:44 -0500

    Fifth example of LPTHW

---
+++ ex5.py
@@ -0,0 +1,15 @@
+my_name='Gonzalo De La Torre'
+my_age= 30
+my_height=1.7 #meters
+my_weight=64 #kilos
+my_eyes='Brown'
+my_teeth='White'
+my_hair='Black'
+
+print "Let's talk about %s." %my_name
+print "He's %f meters tall" %my_height
+print "He's %d kilos heavy" %my_weight
+print "Actually that's not too heavy"
+print "He's got %s eyes and %s hair." %(my_eyes,my_hair)
+print "His teeth are usually %s depending on the coffee." %my_teeth
+print "If I add %d, %f, and %d I get %f" %(my_age, my_height, my_weight, my_age+my_height+my_weight)
