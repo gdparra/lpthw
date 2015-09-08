62b483a41e6517dee9b4695408af48b91aa664ee
Gonzalo <gonzalo_delatorre@live.com>
2015-09-08 13:28:41 -0500

    If, Elif, Else Branches

---
+++ ex31.py
@@ -0,0 +1,31 @@
+print "You enter a dark room with two doors. Do you go through door #1 or #2"
+
+door = raw_input("> ")
+
+if door=="1":
+	print "There is a giant bear eating a cheese cake. What do you do?"
+	print "1. Take the cake"
+	print "2. Scream at the bear"
+
+	bear=raw_input("> ")
+	if bear=="1":
+		print "The bear eats your face off. Good job!"
+	elif bear=="2":
+		print "The bear eats your legs off. Good job!"
+	else:
+		print "Well doing %s is probably better. The bear will run away" %(bear)
+
+elif door=="2":
+	print "you stare into the endless abyss at Cthulu's retina"
+	print "1. Blueberries"
+	print "2. Yellow jacket clothespins"
+	print "3. Understanding revolvers yelling mellodies"
+
+	insanity=raw_input("> ")
+	if insanity =="1" or insanity=="2":
+		print "Your body survives powered by the mind of jellow. Good Job!"
+	else:
+		print "The insanity pools your eyes into a pool of muck. Good job!"
+
+else:
+	print "You stumble around and fall on a knife and die. Good Job!"
